b4c670f6fe916c3e93bafc9f9ea8b72829612e99
bakerjaid <jaidencolebaker@gmail.com>
2018-05-28 20:52:36 +1200

    updated logger to listen to events instead of being called

--- src/jdz/jac/logger/Logger.java
+++ src/jdz/jac/logger/Logger.java
@@ -5,24 +5,18 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
 import jdz.bukkitUtils.fileIO.FileLogger;
 import jdz.jac.JAC;
+import jdz.jac.detection.HackEvent;
+import jdz.jac.punisher.AutobanEvent;
 import lombok.Getter;
 
 public class Logger extends FileLogger {
 	private static final Map<String, Logger> loggers = new HashMap<String, Logger>();
 	@Getter private static final Logger general = new Logger("Hack Log");
-	
-	static {
-		new LoggerConfig().registerEvents(JAC.getInstance());
-	}
-
-	private Logger(String logName) {
-		super(JAC.getInstance(), logName, LoggerConfig.isNewFile());
-		setPrintToConsole(LoggerConfig.isConsoleLogging());
-		setWriteToLog(LoggerConfig.isFileLogging());
-		loggers.put(logName, this);
-	}
 
 	public static Logger get(String logName) {
 		if (!loggers.containsKey(logName))
@@ -32,5 +26,28 @@ public class Logger extends FileLogger {
 
 	public static Collection<Logger> getAll() {
 		return loggers.values();
+	}
+
+	private Logger(String logName) {
+		super(JAC.getInstance(), logName, LoggerConfig.isNewFile());
+		setPrintToConsole(LoggerConfig.isConsoleLogging());
+		setWriteToLog(LoggerConfig.isFileLogging());
+		loggers.put(logName, this);
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onHackEvent(HackEvent event) {
+		general.log(event.getPlayer().getName() + " " + event.getType().getActionDescription() + " "
+				+ event.getExtraData());
+		if (event.isCancelled())
+			general.log("The hack event was cancelled by a plugin");
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onAutoban(AutobanEvent event) {
+		String timeString = event.isPerm() ? "permanently " : "for " + event.getDays() + " days";
+		general.log(event.getPlayer().getName() + " was autobanned " + timeString + "for " + event.getType().getName());
+		if (event.isCancelled())
+			general.log("The autoban event was cancelled by a plugin");
 	}
 }
