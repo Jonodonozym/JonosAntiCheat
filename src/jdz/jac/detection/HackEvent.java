f56bf0b6bfdc58fbb5e9b7b16fcf32abed77d30a
bakerjaid <jaidencolebaker@gmail.com>
2018-05-28 20:52:07 +1200

    update hack event and types

--- src/jdz/jac/detection/HackEvent.java
+++ src/jdz/jac/detection/HackEvent.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
@@ -16,8 +17,15 @@ import lombok.RequiredArgsConstructor;
 public class HackEvent extends Event implements Cancellable{
 	@Getter private final Player player;
 	@Getter private final HackType type;
+	@Getter private String extraData = "";
+	@Getter @Setter private String loggerData = "";
 	@Getter private double persistanceTicksModifier = 1;
 	@Getter private double severityModifier = 1;
+	
+	public HackEvent(Player player, HackType type, String extraData) {
+		this(player, type);
+		this.loggerData = extraData;
+	}
 	
 	@Override
 	public Event call() {
--- src/jdz/jac/detection/HackType.java
+++ src/jdz/jac/detection/HackType.java
@@ -1,27 +1,40 @@
 package jdz.jac.detection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventPriority;
 
 import jdz.bukkitUtils.misc.Config;
 import jdz.jac.JAC;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import net.md_5.bungee.api.chat.TextComponent;
+import lombok.Getter;
 
 @Data
-@RequiredArgsConstructor
-@AllArgsConstructor
 public class HackType {
 	private static final FileConfiguration enabledConfig = Config.getConfig(JAC.getInstance());
-	
+	@Getter private static final Set<HackType> allTypes = new HashSet<HackType>();
+
 	private final String name;
 	private final String actionDescription;
 	private final Severity severity;
 	private final int persistanceTicks;
-	private EventPriority priority = EventPriority.MONITOR;
-	private TextComponent component = null;
+	private final EventPriority priority;
+
+	public HackType(String name, String actionDescription, Severity severity, int persistanceTicks) {
+		this(name, actionDescription, severity, persistanceTicks, EventPriority.MONITOR);
+	}
+
+	public HackType(String name, String actionDescription, Severity severity, int persistanceTicks,
+			EventPriority priority) {
+		this.name = name;
+		this.actionDescription = actionDescription;
+		this.severity = severity;
+		this.persistanceTicks = persistanceTicks;
+		this.priority = priority;
+		allTypes.add(this);
+	}
 
 	public boolean isEnabled() {
 		return enabledConfig.getBoolean(name + ".enabled", true);
