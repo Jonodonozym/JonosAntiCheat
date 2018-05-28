1c0fd6adb5e50b78c8955f17bde339baa2b2788f
bakerjaid <jaidencolebaker@gmail.com>
2018-05-28 20:54:08 +1200

    Added chat utils for formatting messages

---
+++ src/jdz/jac/utils/ChatUtils.java
@@ -0,0 +1,12 @@
+
+package jdz.jac.utils;
+
+import org.bukkit.ChatColor;
+
+public class ChatUtils {
+
+	public static String formatMessage(String s) {
+		return ChatColor.GOLD + "[JAC] " + s;
+	}
+
+}
