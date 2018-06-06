ac841193208780bd3304fa39035d937991619408
bakerjaid <jaidencolebaker@gmail.com>
2018-06-06 12:29:25 +1200

    Added auto-armor detector

--- src/jdz/jac/detection/autoarmor/AutoArmorCheckCommand.java
+++ src/jdz/jac/detection/autoarmor/AutoArmorCheckCommand.java
@@ -1,17 +1,13 @@
 
 package jdz.jac.detection.autoarmor;
 
-import java.util.Set;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import jdz.bukkitUtils.commands.Command;
 import jdz.bukkitUtils.commands.annotations.CommandLabel;
+import jdz.bukkitUtils.commands.annotations.CommandMethod;
 import jdz.bukkitUtils.commands.annotations.CommandPermission;
-import jdz.bukkitUtils.commands.annotations.CommandPlayerOnly;
 import jdz.bukkitUtils.commands.annotations.CommandRequiredArgs;
 import jdz.bukkitUtils.commands.annotations.CommandUsage;
 
@@ -19,26 +15,13 @@ import jdz.bukkitUtils.commands.annotations.CommandUsage;
 @CommandLabel("autoarmorcheck")
 @CommandRequiredArgs(1)
 @CommandUsage("{player}")
-@CommandPlayerOnly
 @CommandPermission("jac.admin")
 public class AutoArmorCheckCommand extends Command {
 
-	@Override
-	public void execute(CommandSender sender, Set<String> flags, String... args) {
-		if (!sender.hasPermission("jac.admin")) {
-			sender.sendMessage(ChatColor.RED+"You are missing the permission node anni.npc");
-			return;
-		}
-		
-		Player player = Bukkit.getPlayer(args[0]);
-		if (player == null || !player.isOnline()) {
-			sender.sendMessage(ChatColor.RED + "That player is not online!");
-			return;
-		}
-
-		sender.sendMessage(ChatColor.GOLD + "[JAC] Checking " + player.getName() + " for auto-armor...");
-
-		AutoArmorVerifier.verify(player);
+	@CommandMethod
+	public void execute(Player sender, Player target) {
+		sender.sendMessage(ChatColor.GOLD + "[JAC] Checking " + target.getName() + " for auto-armor...");
+		AutoArmorVerifier.verify(sender, target);
 	}
 
 }
--- src/jdz/jac/detection/autoarmor/AutoArmorDetector.java
+++ src/jdz/jac/detection/autoarmor/AutoArmorDetector.java
@@ -1,8 +1,61 @@
 
 package jdz.jac.detection.autoarmor;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+
 import jdz.bukkitUtils.events.Listener;
+import jdz.bukkitUtils.misc.utils.MaterialUtils;
+import jdz.jac.JAC;
+import jdz.jac.utils.PlayerPing;
 
-public class AutoArmorDetector implements Listener{
+public class AutoArmorDetector implements Listener {
+	private final Set<Player> equipedOne = new HashSet<Player>();
+	private final Set<Player> checked = new HashSet<Player>();
 
+	@EventHandler
+	public void onPickup(PlayerPickupItemEvent event) {
+		ItemStack stack = event.getItem().getItemStack();
+		if (!MaterialUtils.isArmour(stack.getType()))
+			return;
+
+		Player player = event.getPlayer();
+		if (checked.contains(player))
+			return;
+
+		int ping = PlayerPing.getPing(player);
+		int pingTicks = (int) Math.ceil(ping / 50D);
+		int slot = MaterialUtils.getArmourType(stack.getType()).ordinal();
+		ItemStack item = player.getEquipment().getArmorContents()[slot];
+
+		if (stack.equals(item))
+			return;
+
+		Bukkit.getScheduler().runTaskLater(JAC.getInstance(), () -> {
+			if (!stack.equals(player.getEquipment().getArmorContents()[slot]))
+				return;
+
+			if (!equipedOne.remove(player)) {
+				equipedOne.add(player);
+				return;
+			}
+
+			Bukkit.getScheduler().runTaskLater(JAC.getInstance(), () -> {
+				AutoArmorVerifier.verify(player);
+			}, pingTicks);
+
+			checked.add(player);
+			Bukkit.getScheduler().runTaskLater(JAC.getInstance(), () -> {
+				checked.remove(player);
+			}, 600L);
+
+		}, AutoArmorConfig.getAutoequipTicks() + (AutoArmorConfig.isAddPingToTicks() ? pingTicks : ping));
+
+	}
 }
