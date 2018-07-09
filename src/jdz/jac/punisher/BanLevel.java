1f260b44a9ac53f7599cd590b6c2d047cfdfc5d4
bakerjaid <jaidencolebaker@gmail.com>
2018-07-10 10:03:23 +1200

    Minor code smell

--- src/jdz/jac/punisher/BanLevel.java
+++ src/jdz/jac/punisher/BanLevel.java
@@ -5,7 +5,7 @@ public enum BanLevel {
 	UNIQUE, LIGHT, MEDIUM, HEAVY, PERMANENT;
 
 	public boolean isTemp() {
-		return this == LIGHT || this == MEDIUM || this.equals(HEAVY);
+		return this == LIGHT || this == MEDIUM || this == HEAVY;
 	}
 
 	public static BanLevel[] temp() {
