56aa95338d7381d9333d2471d140837db4311bd6
bakerjaid <jaidencolebaker@gmail.com>
2018-12-23 17:33:12 +1300

    Removed onConfigReload - performed by superclass

--- src/jdz/jac/detection/autoarmor/AutoArmorConfig.java
+++ src/jdz/jac/detection/autoarmor/AutoArmorConfig.java
@@ -1,13 +1,9 @@
 
 package jdz.jac.detection.autoarmor;
 
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.Plugin;
 
 import jdz.bukkitUtils.config.AutoConfig;
-import jdz.bukkitUtils.events.custom.ConfigReloadEvent;
-import jdz.jac.JAC;
 import lombok.Getter;
 
 public class AutoArmorConfig extends AutoConfig {
@@ -18,16 +14,4 @@ public class AutoArmorConfig extends AutoConfig {
 	public AutoArmorConfig(Plugin plugin) {
 		super(plugin, "autoarmor");
 	}
-	
-	@EventHandler
-	public void onConfigReload(ConfigReloadEvent event) {
-		if (!event.getPlugin().equals(JAC.getInstance()))
-			return;
-
-		FileConfiguration config = event.getConfig();
-		enabled = config.getBoolean("autoarmor.enabled", true);
-		autoequipTicks = config.getInt("autoarmor.autoequipTicks", 5);
-		addPingToTicks = config.getBoolean("autoarmor.addPingToTicks", true);
-	}
-
 }
