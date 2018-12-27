d9a936857dd6bba64e3fc4a87f6d5c869edd3ef7
bakerjaid <jaidencolebaker@gmail.com>
2018-12-27 21:42:26 +1300

    changed chat format

--- src/jdz/jac/utils/Messager.java
+++ src/jdz/jac/utils/Messager.java
@@ -10,9 +10,13 @@ public class Messager {
 		sender.sendMessage(format(message));
 	}
 
+	public static void error(CommandSender sender, String message) {
+		message(sender, RED + message);
+	}
+
 	private static String[] format(String message) {
 		String[] lines = message.split("\n");
-		lines[0] = GOLD + "[" + BOLD + "JAC" + RESET + RED + "]" + " " + lines[0];
+		lines[0] = RED + "" + BOLD + "[" + GOLD + "JAC" + RED + "]" + GOLD + RESET + " " + lines[0];
 		for (int i = 1; i < lines.length; i++)
 			lines[i] = lines[i];
 		return lines;
