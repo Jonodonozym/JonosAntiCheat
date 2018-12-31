6ddfe309dba0c44a75918e381b98ad5ad2c6bb71
bakerjaid <jaidencolebaker@gmail.com>
2018-12-31 16:29:03 +1300

    auto-adds enabled field

--- src/jdz/jac/detection/TypeEnabledConfig.java
+++ src/jdz/jac/detection/TypeEnabledConfig.java
@@ -1,6 +1,9 @@
 
 package jdz.jac.detection;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 
@@ -12,13 +15,30 @@ import jdz.jac.JAC;
 public class TypeEnabledConfig implements Listener {
 	private static FileConfiguration config = Config.getConfig(JAC.getInstance());
 
+	public static boolean isEnabled(HackType type) {
+		return config.getBoolean(configKey(type), true);
+	}
+
+	private static String configKey(HackType type) {
+		return type.getName().toLowerCase() + ".enabled";
+	}
+
 	@EventHandler
 	public void configReload(ConfigReloadEvent event) {
 		if (event.getPlugin().equals(JAC.getInstance()))
 			config = event.getConfig();
+		addDefaultSections(config, Config.getConfigFile(JAC.getInstance(), event.getName()));
 	}
 
-	public static boolean isEnabled(HackType type) {
-		return config.getBoolean(type.getName().toLowerCase() + ".enabled", true);
+	private static void addDefaultSections(FileConfiguration config, File file) {
+		for (HackType type : HackType.getAllTypes())
+			if (!config.contains(configKey(type)))
+				config.set(configKey(type), false);
+		try {
+			config.save(file);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
